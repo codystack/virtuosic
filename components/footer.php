8247b666a5906312b340654e2a12b5e9688d2387
ThankGod Okoro <thankgodokoro@ThankGods-MBP.lan>
2022-08-09 19:02:13 +0100

    quote link added

--- components/footer.php
+++ components/footer.php
@@ -2,38 +2,15 @@
   <footer class="py-15 py-xl-10 bg-black inverted">
     <div class="container">
       <div class="row g-4 g-lg-5 mb-10">
-        <div class="col-md-3 text-center text-md-start">
+        <div class="col-md-12 text-center">
           <a href="./" class="navbar-brand"><img src="assets/images/virtlogowhite.png" alt="Logo"></a>
-        </div>
-        <div class="col-md-4 col-lg-5 text-center text-md-start">
-          <ul class="list-unstyled">
-            <li class="mb-1"><a href="" class="text-reset text-primary-hover">documentation</a></li>
-            <li class="mb-1"><a href="" class="text-reset text-primary-hover">components</a></li>
-            <li class="mb-1"><a href="" class="text-reset text-primary-hover">blocks</a></li>
-            <li class="mb-1"><a href="" class="text-reset text-primary-hover">changelog</a></li>
-            <li><a href="" class="text-reset text-primary-hover">roadmap</a></li>
-          </ul>
-        </div>
-        <div class="col-md-5 col-lg-4">
-          <div class="grouped-inputs border p-1 rounded-pill">
-            <div class="row">
-              <div class="col">
-                <input type="text" class="form-control rounded-pill px-3" aria-label="Text input"
-                  placeholder="Your email">
-              </div>
-              <div class="col-auto">
-                <a href="" class="btn btn-white btn-icon rounded-circle"><i class="bi bi-arrow-return-left"></i></a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <div class="row align-items-center g-1 g-lg-6 text-muted">
         <div class="col-md-6 col-lg-6 order-lg-3 text-center text-md-end">
           <ul class="list-inline small">
-            <li class="list-inline-item"><a href="" class="text-reset text-primary-hover">facebook</a></li>
-            <li class="list-inline-item ms-1"><a href="" class="text-reset text-primary-hover">twitter</a></li>
-            <li class="list-inline-item ms-1"><a href="" class="text-reset text-primary-hover">linkedin</a></li>
+            <li class="list-inline-item"><a href="" class="text-reset text-primary-hover">Terms of Use</a></li>
+            <li class="list-inline-item ms-1"><a href="" class="text-reset text-primary-hover">Privacy Policy</a></li>
           </ul>
         </div>
         <div class="col-lg-6 order-lg-1 text-center text-md-start">
--- quote.php
+++ quote.php
@@ -3,7 +3,112 @@ include "./components/header.php";
 include "./components/navbarlight.php";
 ?>
 
-
+<section class="py-15 py-xl-20 border-top">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 mb-5">
+          <h2>Let's make something big. <span class="d-block">Together.</span></h2>
+        </div>
+        <div class="col-lg-8">
+          <form action="#" class="row g-3">
+            <div class="col-md-6">
+              <label for="firstName" class="form-label">First Name</label>
+              <input type="text" class="form-control" required name="firstName" placeholder="First Name">
+            </div>
+            <div class="col-md-6">
+              <label for="lastName" class="form-label">Last Name</label>
+              <input type="text" class="form-control" required name="lastName" placeholder="Last Name">
+            </div>
+            <div class="col-md-6">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" required name="email" placeholder="Email Address">
+            </div>
+            <div class="col-md-6">
+              <label for="phone" class="form-label">Phone Number</label>
+              <input type="tel" class="form-control" required name="phone" placeholder="Phone Number">
+            </div>
+            <div class="col-md-6">
+              <label for="jobTitle" class="form-label">Job Title</label>
+              <input type="text" class="form-control" required name="jobTitle" placeholder="Job Title">
+            </div>
+            <div class="col-md-6">
+              <label for="company" class="form-label">Company</label>
+              <input type="text" class="form-control" required name="company" placeholder="Company">
+            </div>
+            <div class="col-md-6">
+              <label for="billingAddress" class="form-label">Billing Address</label>
+              <input type="text" class="form-control" required name="billingAddress" placeholder="Billing Address">
+            </div>
+            <div class="col-md-6">
+              <label for="address" class="form-label">Address</label>
+              <input type="text" class="form-control" required name="address" placeholder="Address">
+            </div>
+            <div class="col-md-6">
+              <label for="city" class="form-label">City</label>
+              <input type="text" class="form-control" required name="city" placeholder="City">
+            </div>
+            <div class="col-md-6">
+              <label for="country" class="form-label">Country</label>
+              <input type="text" class="form-control" required name="country" placeholder="Country">
+            </div>
+            <div class="col-md-6">
+              <label for="stateProvinceRegion" class="form-label">State / Province / Region</label>
+              <input type="text" class="form-control" required name="stateProvinceRegion" placeholder="State / Province / Region">
+            </div>
+            <div class="col-md-6">
+              <label for="postalZipCode" class="form-label">Postal / Zip Code</label>
+              <input type="text" class="form-control" required name="postalZipCode" placeholder="Postal / Zip Code">
+            </div>
+            <div class="col-md-6">
+              <label for="role" class="form-label">What is your role?</label>
+              <input type="text" class="form-control" required name="role" placeholder="What is your role?">
+            </div>
+            <div class="col-md-6">
+              <label for="companyIndustry" class="form-label">What industry is your company in?</label>
+              <input type="text" class="form-control" required name="companyIndustry" placeholder="What industry is your company in?">
+            </div>
+            <div class="col-md-6">
+              <label for="inquiryNature" class="form-label">What is the nature of your inquiry?</label>
+              <input type="text" class="form-control" required name="inquiryNature" placeholder="What is the nature of your inquiry?">
+            </div>
+            <div class="col-md-6">
+              <label for="budget" class="form-label">What is your approximate budget?</label>
+              <input type="text" class="form-control" required name="budget" placeholder="What is your approximate budget?">
+            </div>
+            <div class="col-md-6">
+              <label for="timeFrame" class="form-label">What is the timeframe to implement?</label>
+              <input type="text" class="form-control" required name="timeFrame" placeholder="What is the timeframe to implement?">
+            </div>
+            <div class="col-md-6">
+              <label for="requestUrgency" class="form-label">What is the urgency of your request?</label>
+              <input type="text" class="form-control" required name="requestUrgency" placeholder="What is the urgency of your request?">
+            </div>
+            <div class="col-md-12">
+              <label for="referral" class="form-label">How did you hear about us?</label>
+              <input type="text" class="form-control" required name="referral" placeholder="How did you hear about us?">
+            </div>
+            <div class="col-md-12">
+              <label for="comment" class="form-label">Any comments?</label>
+              <textarea class="form-control" required name="comment" rows="3" placeholder="Any comments?"></textarea>
+            </div>
+            <div class="col-md-8">
+              <div class="form-check">
+                <input class="form-check-input" required type="checkbox" id="gridCheck">
+                <label class="form-check-label text-muted small" for="gridCheck">
+                  I accept the <a href="" class="underline">Terms
+                    & Conditions</a> and acknowledge that my information will be used in accordance with Privacy
+                  Policy.
+                </label>
+              </div>
+            </div>
+            <div class="col-md-12 d-grid">
+              <a href="" class="btn btn-block btn-black">Request Quote</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
 <?php include "./components/footer.php";?>
